68350deddfadb13fd8c523a91c1b8246ca0f8915
Tayfun Kuşçu <69037566+tayfunkscu@users.noreply.github.com>
2021-04-17 19:30:05 +0300

    main page updated

--- google_maps_in_flutter/lib/main.dart
+++ google_maps_in_flutter/lib/main.dart
@@ -1,13 +1,113 @@
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
-void main() => runApp(MyApp());
-
-class MyApp extends StatefulWidget {
-  @override
-  _MyAppState createState() => _MyAppState();
+void main() {
+  runApp(MaterialApp(
+    title: 'Navigation Basics',
+    home: FirstRoute(),
+  ));
 }
 
+class FirstRoute extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        theme: ThemeData(
+          primarySwatch: Colors.amber,
+        ),
+        home: Scaffold(
+          body: Center(
+              child: Column(children: <Widget>[
+                Container(
+                  child: Padding(
+                    padding: EdgeInsets.fromLTRB(20, 100, 20, 0),
+                    child: Image(
+                      image: AssetImage('assets/mainLogo.png'),
+                      height: 300,
+                      width: 300,
+                    ),
+                  ),
+                ),
+                Container(
+                    margin: EdgeInsets.fromLTRB(20.0,50,20.0,0),
+                    child: ElevatedButton(
+                      child: Padding(
+                        padding: EdgeInsets.fromLTRB(50, 0, 50, 0),
+                        child: Text('Query 1'),
+                      ),
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(builder: (context) => SecondRoute()),
+                        );
+                      },
+                      style: ElevatedButton.styleFrom(
+                        shape: new RoundedRectangleBorder(
+                          borderRadius: new BorderRadius.circular(20.0),
+                        ),
+                      ),
+                    )),
+                Container(
+                    child: ElevatedButton(
+                      child: Padding(
+                        padding: EdgeInsets.fromLTRB(50, 0, 50, 0),
+                        child: Text('Query 2'),
+                      ),
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(builder: (context) => SecondRoute()),
+                        );
+                      },
+                      style: ElevatedButton.styleFrom(
+                        shape: new RoundedRectangleBorder(
+                          borderRadius: new BorderRadius.circular(20.0),
+                        ),
+                      ),
+                    )),
+                Container(
+                    child: ElevatedButton(
+                      child: Padding(
+                        padding: EdgeInsets.fromLTRB(50, 0, 50, 0),
+                        child: Text('Query 3'),
+                      ),
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(builder: (context) => SecondRoute()),
+                        );
+                      },
+                      style: ElevatedButton.styleFrom(
+                        shape: new RoundedRectangleBorder(
+                          borderRadius: new BorderRadius.circular(20.0),
+                        ),
+                      ),
+                    )),
+              ])),
+        ));
+  }
+}
+
+class SecondRoute extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Second Route"),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          child: Text('Go back!'),
+        ),
+      ),
+    );
+  }
+}
+
+/*
 class _MyAppState extends State<MyApp> {
   late GoogleMapController mapController;
 
@@ -22,7 +122,7 @@ class _MyAppState extends State<MyApp> {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text('Maps Sample App'),
+          title: Text('Maps'),
           backgroundColor: Colors.green[700],
         ),
         body: GoogleMap(
@@ -36,3 +136,4 @@ class _MyAppState extends State<MyApp> {
     );
   }
 }
+*/
